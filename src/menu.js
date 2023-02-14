05606b55e8f91307fb7d50ea8f673374d788023b
Ujwal Vinay <walkerdoodle7@gmail.com>
2023-02-14 23:30:08 +0530

    Menu list added

---
+++ src/menu.js
@@ -0,0 +1,25 @@
+const MenuData = [
+    {
+        id:1,
+        item1 : "Beef Steak",
+        img1 : "../images/menu/beef-steak.svg",
+        item2 : "Spagetti",
+        img2 : "../images/menu/spagetti.svg"
+    },
+    {
+        id:2,
+        item1 : "Veg Salad",
+        img1 : "../images/menu/veg-salad.svg",
+        item2 : "Fried Chicken Noodle",
+        img2 : "../images/menu/fried-chicken-noodles.svg"
+    },
+    {
+        id:1,
+        item1 : "Sushi Delight",
+        img1 : "../images/menu/sushi.svg",
+        item2 : "Seaser Salad",
+        img2 : "../images/menu/seaser-salad.svg"
+    }
+]
+
+export default MenuData;
